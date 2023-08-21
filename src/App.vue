90f11b8873da06dd02de325f4f9f16eacb771c74
premod <premodsuraweera1@gmail.com>
2023-08-21 17:58:04 +0530

    pinia use

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+import TaskDetails from "./components/TaskDetails.vue";
+import TaskForm from "./components/TaskForm.vue";
+import { useTaskStore } from "./stores/TaskStore";
+import { ref } from "vue";
+
+
+const filter = ref('all');
+const taskStore = useTaskStore();
+</script>
+
+<template>
+  <main>
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="pinia logo" />
+      <h1>Pinia Tasks</h1>
+    </header>
+
+    <!-- new task form -->
+    <div class="new-task-form">
+      <TaskForm/>
+    </div>
+
+    <!-- filter -->
+    <nav class="filter">
+      <button @click="filter = 'all'">All tasks</button>
+      <button @click="filter = 'favs'">fav tasks</button>
+    </nav>
+
+    <!-- TaskList -->
+    <div class="task-list" v-if="filter === 'all'">
+      <p>All Task {{ taskStore.totalCount }}</p>
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+    <div class="task-list" v-if="filter === 'favs'">
+        <p>Fav Task {{ taskStore.favCount }}</p>
+      <div v-for="task in taskStore.favs" :key="task.id">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/TaskDetails.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { useTaskStore } from '../stores/TaskStore';
+
+
+const taskStore = useTaskStore()
+
+const props = defineProps({
+    task: { 
+        type: Object,
+        required: true,
+    }
+})
+
+</script>
+<template>
+    <div class="task">
+        <h2>{{ task.title }}</h2>
+        <div class="icons">
+            <i class="material-icons" @click="taskStore.deleteTask(task.id)">delete</i>
+            <i class="material-icons" :class="{active:task.isFav}" @click="taskStore.toggleFav(task.id)">favorite</i>
+        </div>
+    </div>
+</template>
